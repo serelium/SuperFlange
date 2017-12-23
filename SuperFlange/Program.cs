95099d4d98537cd020307e83cd200924685787a6
Phantom <Phantom@DESKTOP-SKH9SN7>
2017-12-23 14:36:58 -0500

    Add project files.

---
+++ SuperFlange/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperFlange
+{
+    class Program
+    {
+        /// <summary>
+        /// Application Entry Point.
+        /// </summary>
+        [System.STAThreadAttribute()]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.0.0.0")]
+        public static void Main()
+        {
+            SuperFlange.App app = new SuperFlange.App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
---
+++ SuperFlange/ViewModel/FlangeViewModel.cs
@@ -0,0 +1,31 @@
+﻿using SuperFlange.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperFlange.ViewModel
+{
+    class FlangeViewModel : ViewModelBase
+    {
+        private Flange _Flange;
+
+        public FlangeViewModel()
+        {
+            Flange _Flange = new Flange();
+            _Flange.PropertyChanged += new PropertyChangedEventHandler(Flange_PropertyChanged);
+        }
+
+        private void Flange_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            switch (e.PropertyName)
+            {
+                case nameof(_Flange.Thickness):
+                    RaisePropertyChangedEvent(e.PropertyName);
+                    break;
+            }
+        }
+    }
+}
